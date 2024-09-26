9cc09fd02898cfd76efb428b750cc87e791d4743
LangatD <dorcaschepngetich20@gmail.com>
2024-09-26 11:10:25 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Code your solution in this file!
+// Code let your solution in this file!
+
+const companyName = 'Scuber'
+let mostProfitableNeighborhood = "Chelsea"
+let companyCeo = "Susan Smith"
